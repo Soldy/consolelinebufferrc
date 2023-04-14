4b72a036aaa2e254e659caef66167b61f656d8a8
Soldy <4786022+Soldy@users.noreply.github.com>
2023-04-14 23:51:36 +0100

    code update for node 18 & 19
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const nanoTest  = new (require('nanoTest')).test({
-    'debugPrint' : 'short'
+    'progress_bar' : false,
+    'debug_print' : 'short'
 });
 const lineBufferRc = new (require('./index.js')).base();
 
